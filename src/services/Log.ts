cfb388a67b3f9482b1f61220fd36e60c867eeb63
Brian Perrin <perrin@live.com>
2026-07-07 10:03:32 -0700

    Log class Add pino dependency. Create Log class.

---
+++ src/services/Log.ts
@@ -0,0 +1,40 @@
+import pino from 'pino';
+
+const logger = pino({
+    formatters: {
+        level: label => {
+            return { level: label };
+        },
+    },
+}, pino.transport({
+    target: 'pino-pretty',
+    options: {
+        colorize: true,
+    },
+}));
+
+export class Log {
+    public static info(message: string, obj?: any): void {
+        if (obj) {
+            logger.info(obj, message);
+        } else {
+            logger.info(message);
+        }
+    }
+
+    public static warn(message: string, obj?: any): void {
+        if (obj) {
+            logger.warn(obj, message);
+        } else {
+            logger.warn(message);
+        }
+    }
+
+    public static error(message: string, obj?: any): void {
+        if (obj) {
+            logger.error(obj, message);
+        } else {
+            logger.error(message);
+        }
+    }
+}
